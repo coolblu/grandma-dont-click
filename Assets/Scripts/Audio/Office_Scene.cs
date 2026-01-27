6972004f531d93061e77fe181dfc3c1f7a3d766e
Emma Albright <e.albright33@gmail.com>
2026-01-27 14:20:28 -0600

    Footsteps

--- Assets/Scripts/Audio/Office_Scene.cs
+++ Assets/Scripts/Audio/Office_Scene.cs
@@ -1,23 +1,38 @@
 using UnityEngine;
 using System.Collections;
-using System.Collections.Generic;
-using static PhoneKeypadUI;
+using static FirstPersonController;
 
 public class Office_Scene : MonoBehaviour
 {
+    [Header("Scene Audio")]
     public AudioSource audio;
     public AudioClip RingingClip;
     public AudioClip ScamClip;
     public PhoneKeypadUI keypad;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [Header("Footsteps")]
+    public FirstPersonController controller;
+    public AudioSource footsteps;
+    public float minSpeed = 0.1f;
+
+    private Vector3 lastPosition;
+
     void Start()
     {
-        // Start the coroutine when the object starts
         StartCoroutine(ExecuteAfterTime(2.0f));
+        lastPosition = transform.position;
     }
 
     void Update()
+    {
+        HandlePhoneAudio();
+        HandleFootsteps();
+    }
+
+    // -----------------------------
+    // PHONE AUDIO LOGIC
+    // -----------------------------
+    void HandlePhoneAudio()
     {
         if (keypad.IsOpen && audio.clip == RingingClip)
         {
@@ -26,23 +41,44 @@ public class Office_Scene : MonoBehaviour
         }
     }
 
+    // -----------------------------
+    // FOOTSTEP LOGIC
+    // -----------------------------
+    void HandleFootsteps()
+    {
+        float speed = (transform.position - lastPosition).magnitude / Time.deltaTime;
+        lastPosition = transform.position;
+
+        if (speed > minSpeed && controller.IsGrounded)
+        {
+            if (!footsteps.isPlaying)
+                footsteps.Play();
+        }
+        else
+        {
+            if (footsteps.isPlaying)
+                footsteps.Stop();
+        }
+    }
+
+    // -----------------------------
+    // COROUTINES
+    // -----------------------------
     IEnumerator ExecuteAfterTime(float delay)
     {
-        // Wait for the specified number of seconds
         yield return new WaitForSeconds(delay);
 
-        // Code here will execute after the delay
         audio.clip = RingingClip;
         audio.loop = true;
         audio.Play();
     }
 
-    IEnumerator ScamCall ()
+    IEnumerator ScamCall()
     {
         yield return null;
+
         audio.clip = ScamClip;
         audio.loop = false;
         audio.Play();
     }
-
 }
